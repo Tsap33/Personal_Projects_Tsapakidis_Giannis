cc0daf10dc435f9db56bb4fc3c2869c337d5ffbf
Tsap33 <johnara333@gmail.com>
2025-03-22 00:05:42 +0200

    Add files via upload

---
+++ C#_MSSQL_Project/akmi-ergasia-proodou-logism-2-main/akmi-ergasia-proodou-logism-2-main/Invoicer/NewVendor.cs
@@ -0,0 +1,141 @@
+﻿using SQL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+using static Exam04.Common;
+
+namespace Exam04 {
+	public partial class NewVendor : Form {
+		private readonly Timer pollTimer;
+		private readonly SQL_Connector sql;
+
+		public NewVendor(SQL_Connector sql) {
+			InitializeComponent();
+			this.sql = sql;
+
+			pollTimer = new Timer { Interval = 25 };
+			pollTimer.Tick += FormStatusCheckSQL;
+			pollTimer.Start();
+		}
+
+		~NewVendor() {
+			pollTimer.Stop();
+			pollTimer.Dispose();
+			if (DialogResult != DialogResult.OK) {
+				DialogResult = DialogResult.Cancel;
+			}
+		}
+
+		private void FormStatusCheckSQL(object sender, EventArgs e) {
+			bool conn = sql.IsValidConnection();
+
+			InsertButton.Enabled = CheckSQL.Checked = conn;
+			CheckSQL.Text = $"SQL Status: {(conn ? "Connected" : "Disconnected")}";
+		}
+
+		private void NewSupplier_Load(object sender, EventArgs e) {
+			StateProvince.Populate(sql, "Name", "StateProvinceID", @"
+				SELECT {} FROM Person.StateProvince;"
+			);
+		}
+
+		private void BackButton_Click(object sender, EventArgs e) {
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+
+		private void InsertButton_Click(object sender, EventArgs e) {
+			if (CheckEmptyFields(
+				CompanyNameField, AccountNumField,
+				CityField, Address1Field, PostalCodeField
+			)) { return; }
+
+			var (_, error) = sql.ExecuteNonQuery(@"
+				BEGIN TRY
+					BEGIN TRANSACTION;
+
+					DECLARE @_BusinessEntityID INT;
+					DECLARE @_AddressID INT;
+
+					INSERT INTO Person.BusinessEntity (rowguid, ModifiedDate)
+						VALUES (NEWID(), GETDATE());
+
+					SELECT TOP 1 @_BusinessEntityID = BusinessEntityID
+						FROM Person.BusinessEntity
+						ORDER BY BusinessEntityID DESC;
+
+					INSERT INTO Person.Address (AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, rowguid, ModifiedDate)
+						VALUES (@AddressLine1, @AddressLine2, @City, @StateProvinceID, @PostalCode, NEWID(), GETDATE());
+
+					SELECT TOP 1 @_AddressID = AddressID
+						FROM Person.Address
+						ORDER BY AddressID DESC;
+
+					INSERT INTO Person.BusinessEntityAddress (BusinessEntityID, AddressID, AddressTypeID, rowguid, ModifiedDate)
+						VALUES (@_BusinessEntityID, @_AddressID, 1, NEWID(), GETDATE());
+
+					INSERT INTO Purchasing.Vendor (BusinessEntityID, AccountNumber, Name, CreditRating, PreferredVendorStatus, ActiveFlag, PurchasingWebServiceURL, ModifiedDate)
+						VALUES (@_BusinessEntityID, @AccountNumber, @CompanyName, 1, 1, 1, NULL, GETDATE());
+
+					COMMIT TRANSACTION;
+				END TRY
+				BEGIN CATCH
+					ROLLBACK TRANSACTION;
+					THROW;
+				END CATCH;",
+
+				new SqlParameter("@CompanyName",     CompanyNameField.Text),
+				new SqlParameter("@AccountNumber",   AccountNumField.Text),
+				new SqlParameter("@StateProvinceID", StateProvince.SelectedIndex + 1),
+				new SqlParameter("@City",            CityField.Text),
+				new SqlParameter("@AddressLine1",    Address1Field.Text),
+				new SqlParameter("@AddressLine2",    Address2Field.Text),
+				new SqlParameter("@PostalCode",      PostalCodeField.Text)
+			);
+
+			if (error == 0) {
+				MessageBox.Show("Vendor successfully inserted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				DialogResult = DialogResult.OK;
+				Close();
+			}
+		}
+
+		private void CheckSQL_Click(object sender, EventArgs e) {
+			FormStatusCheckSQL(this, EventArgs.Empty);
+		}
+
+		private void CompanyNameField_TextChanged(object sender, EventArgs e) {
+			CompanyNameField.CheckRequired();
+		}
+
+		private void AccountNumField_TextChanged(object sender, EventArgs e) {
+			AccountNumField.CheckRequired();
+		}
+
+		private void CityField_TextChanged(object sender, EventArgs e) {
+			CityField.CheckRequired();
+		}
+
+		private void Address1Field_TextChanged(object sender, EventArgs e) {
+			Address1Field.CheckRequired();
+		}
+
+		private void PostalCodeField_TextChanged(object sender, EventArgs e) {
+			PostalCodeField.CheckRequired();
+		}
+	}
+}
---
+++ C#_MSSQL_Project/akmi-ergasia-proodou-logism-2-main/akmi-ergasia-proodou-logism-2-main/Invoicer/SqlConnector.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL {
+	public enum EXCEPTION : int {
+		NONE = 0, CONN = 1, ELSE = 2
+	}
+
+	public class SQL_Connector {
+		private SqlConnection mConnLink;
+		private bool mConnValid = false;
+		private int  mConnCount = 0;
+		private int  mTimeOut   = 5;
+
+		public int  ConnectionsActive => mConnCount;
+		public bool HasConnection     => mConnValid;
+		public int CommandTimeout {
+			get => mTimeOut;
+			set => mTimeOut = value < 0 ? 0 : value;
+		}
+
+		private void LogSqlError(Exception e, string caption = "") {
+#if DEBUG
+			MessageBox.Show(e.Message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+#else
+			Console.WriteLine(caption + Environment.NewLine + e.Message);
+#endif
+		}
+
+		private SqlCommand CreateCommand(string query, SqlParameter[] parameters) {
+			var command = new SqlCommand(query, mConnLink);
+			command.Parameters.AddRange(parameters);
+			command.CommandTimeout = mTimeOut;
+			return command;
+		}
+
+		public SQL_Connector() { }
+		public SQL_Connector(string data, int timeout = 5) {
+			_ = InitConnData(data);
+			mTimeOut = timeout;
+		}
+
+		public bool IsValidConnection() { return mConnValid; }
+
+		public (bool Success, int Error) OpenConnection() {
+			if (!mConnValid) { return (false, -1); }
+			try {
+				if (mConnLink.State != ConnectionState.Open) {
+					if (mConnCount == 0) { mConnLink.Open(); }
+				}
+				if (mConnLink.State == ConnectionState.Open) { ++mConnCount; }
+				return (mConnLink.State == ConnectionState.Open, 0);
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: OpenConnection()");
+				return (false, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: OpenConnection()");
+				return (false, -2);
+			}
+		}
+		public (bool Success, int Error) CloseConnection() {
+			if (!mConnValid) { return (false, -1); }
+			try {
+				if (mConnLink?.State != ConnectionState.Closed) {
+					if (mConnCount == 1) { mConnLink?.Close(); }
+					if (mConnCount >= 1) { --mConnCount; }
+				}
+				return (mConnLink?.State == ConnectionState.Closed, 0);
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: CloseConnection()");
+				return (false, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: CloseConnection()");
+				return (false, -2);
+			}
+		}
+
+		public (bool Success, int Error) InitConnData(string data) {
+			try {
+				if (mConnLink?.State != ConnectionState.Closed) {
+					mConnLink?.Close();
+				}
+				mConnLink?.Dispose();
+				mConnLink = new SqlConnection(Regex.Replace(data, @"\s+", " "));
+
+				mConnLink.Open();
+				mConnLink.Close();
+				return (true, 0);
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: InitConnData()");
+				mConnLink?.Dispose();
+				mConnLink = null;
+				return (false, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: InitConnData()");
+				mConnLink?.Dispose();
+				mConnLink = null;
+				return (false, -2);
+			} finally {
+				mConnValid = mConnLink != null;
+			}
+		}
+
+		public (int Value, int Error) ExecuteNonQuery(string query, params SqlParameter[] parameters) {
+			if (!mConnValid) { return (0, -1); }
+			try {
+				var (success, error) = OpenConnection();
+				if (!success || error != 0) { return (0, error); }
+				using (var command = CreateCommand(query, parameters)) {
+					return (command.ExecuteNonQuery(), 0);
+				}
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: ExecuteNonQuery()");
+				return (0, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: ExecuteNonQuery()");
+				return (0, -2);
+			} finally {
+				CloseConnection();
+			}
+		}
+
+		public (object Value, int Error) ExecuteScalarQuery(string query, params SqlParameter[] parameters) {
+			if (!mConnValid) { return (null, -1); }
+			try {
+				var (success, error) = OpenConnection();
+				if (!success || error != 0) { return (0, error); }
+				using (var command = CreateCommand(query, parameters)) {
+					return (command.ExecuteScalar(), 0);
+				}
+			} catch (SqlException e) {
+				if (e.Number == 0) { throw; }
+				LogSqlError(e, "SQL :: ExecuteScalarQuery()");
+				return (null, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: ExecuteScalarQuery()");
+				return (null, 1);	
+			} finally {
+				CloseConnection();
+			}
+		}
+
+		private (object, int Error) FillDataTable(DataTable table, string query, params SqlParameter[] parameters) {
+			if (!mConnValid) { return (null, -1); }
+			try {
+				var (success, error) = OpenConnection();
+				if (!success || error != 0) { return (null, error); }
+				using (var command = CreateCommand(query, parameters)) {
+					using (var adapter = new SqlDataAdapter(command)) {
+						adapter.Fill(table);
+						return (null, 0);
+					}
+				}
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: FillExistingDataTable()");
+				return (null, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: FillExistingDataTable()");
+				return (null, -2);
+			} finally {
+				CloseConnection();
+			}
+		}
+
+		public int FillExistingDataTable(DataTable table, string query, params SqlParameter[] parameters) {
+			if (table == null) { return 0; }
+			return FillDataTable(table, query, parameters).Error;
+		}
+
+		public (DataTable Value, int Error) CreateFilledDataTable(string query, params SqlParameter[] parameters) {
+			var table = new DataTable();
+			var error = FillDataTable(table, query, parameters).Error;
+			return (error == 0 ? table : null, error);
+		}
+
+		public (SqlDataReader Value, int Error) CreateDataReader(string query, params SqlParameter[] parameters) {
+			if (!mConnValid) { return (null, -1); }
+			try {
+				var (success, error) = OpenConnection();
+				if (!success || error != 0) { return (null, error); }
+				using (var command = CreateCommand(query, parameters)) {
+					return (command.ExecuteReader(), 0);
+				}
+			} catch (SqlException e) {
+				LogSqlError(e, "SQL :: CreateDataReader()");
+				return (null, e.Number);
+			} catch (Exception e) {
+				LogSqlError(e, "SQL :: CreateDataReader()");
+				return (null, -2);
+			}
+			// no CloseConnection() here, call explicitly as
+			// an active connection is required to consume!
+		}
+	}
+}
